940d15bd9f75de62f36ce40b16597fb499475c13
Nathan Sehatzadeh <Nathan.Sehatzadeh@gmail.com>
2026-03-28 16:17:58 -0400

    Complete Week 3 concert CRUD implementation

--- CTMS/Controllers/ConcertController.cs
+++ CTMS/Controllers/ConcertController.cs
@@ -1,18 +1,97 @@
-namespace CTMS.Controllers;
-
 using Microsoft.AspNetCore.Mvc;
+using CTMS.Data;
 using CTMS.Models;
-using System.Collections.Generic;
 
-public class ConcertController : Controller
+namespace CTMS.Controllers
 {
-    public IActionResult Index()
+    public class ConcertController : Controller
     {
-        var concerts = new List<Concert>
-        {
-            new Concert { Id = 1, Title = "Metal Night", Location = "Toronto", Date = DateTime.Now }
-        };
+        private readonly ApplicationDbContext _context;
 
-        return View(concerts);
+        public ConcertController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // READ (List)
+        public IActionResult Index()
+        {
+            var concerts = _context.Concerts.ToList();
+            return View(concerts);
+        }
+
+        // CREATE (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE (POST)
+        [HttpPost]
+        public IActionResult Create(Concert concert)
+        {
+            Console.WriteLine("POST Create reached");
+
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("ModelState is invalid");
+
+                foreach (var error in ModelState)
+                {
+                    Console.WriteLine($"Field: {error.Key}");
+                    foreach (var err in error.Value.Errors)
+                    {
+                        Console.WriteLine($"Error: {err.ErrorMessage}");
+                    }
+                }
+
+                return View(concert);
+            }
+
+            _context.Concerts.Add(concert);
+            _context.SaveChanges();
+
+            Console.WriteLine("Concert saved");
+            return RedirectToAction("Index");
+        }
+
+        // EDIT (GET)
+        public IActionResult Edit(int id)
+        {
+            var concert = _context.Concerts.Find(id);
+            if (concert == null) return NotFound();
+            return View(concert);
+        }
+
+        // EDIT (POST)
+        [HttpPost]
+        public IActionResult Edit(Concert concert)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Concerts.Update(concert);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(concert);
+        }
+
+        // DELETE (GET)
+        public IActionResult Delete(int id)
+        {
+            var concert = _context.Concerts.Find(id);
+            if (concert == null) return NotFound();
+            return View(concert);
+        }
+
+        // DELETE (POST)
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var concert = _context.Concerts.Find(id);
+            _context.Concerts.Remove(concert);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- CTMS/Models/Concert.cs
+++ CTMS/Models/Concert.cs
@@ -3,7 +3,7 @@ namespace CTMS.Models;
 public class Concert
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Location { get; set; }
+    public string? Title { get; set; }
+    public string? Location { get; set; }
     public DateTime Date { get; set; }
 }
